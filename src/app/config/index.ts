95d19b8747a793a1e79540b86e6052e3999f63b1
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-08-05 06:12:44 +0600

    custom error handler, added utility functions, and not found middlewares

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,7 +4,7 @@ import path from "path";
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export const config = {
-  env: process.env.NODE_ENV || "development",
+  env: process.env.NODE_ENV,
   port: process.env.PORT,
   dbUri: process.env.DB_URI as string,
 };
