498d0a903b00418d37d5fe6c7c461dc048dd0753
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-08-10 10:17:58 +0600

    vercel setup and hosting done

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,6 @@ export const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(morgan('dev'));
 
 app.use("/api/v1", router);
 
